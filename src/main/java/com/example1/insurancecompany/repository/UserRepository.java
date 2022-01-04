87ca67b8dde011574907fc122fad54aebb8c481a
tomeczek2886 <tomeczek2886@gmail.com>
2022-01-04 18:43:42 +0100

    feat: create repositories

---
+++ src/main/java/com/example1/insurancecompany/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example1.insurancecompany.repository;
+
+import com.example1.insurancecompany.domain.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+        @Query("Select u From User u Order By u.id Asc")
+        List<User> findUsers(Pageable pageable);
+
+}
